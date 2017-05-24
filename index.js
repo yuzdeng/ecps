e4362825a3ff0504e6e2118f0b14a7cd01eaae28
yuzdeng <yuzdeng@aliyun.com>
2017-05-24 19:09:13 +0800

    delete the useless function and expose a API for AMD/CMD

---
+++ index.js
@@ -0,0 +1,14 @@
+var Build = require('./lib/build');
+var Util = require(__dirname + '/util');
+
+module.exports={
+    build : function (args,config) {
+        var arguments = Util.getConfigArgs(args,config);
+
+        if (arguments.config === null) {
+            Util.error('File not found: ecps-config.js');
+            return false;
+        }
+        Build(arguments.args, arguments.config);
+    }
+}
